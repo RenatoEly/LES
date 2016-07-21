707a232cbf3081de3d03ca1718b6b423b35a8bf0
Renato Ely <renato.silva@ccc.ufcg.edu.br>
2016-07-20 22:03:16 -0300

    teste de cores

--- app/src/main/java/com/example/renato/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/renato/myapplication3/MainActivity.java
@@ -4,16 +4,19 @@ import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.db.chart.model.Bar;
 import com.db.chart.model.BarSet;
 import com.db.chart.view.BarChartView;
+import com.db.chart.view.animation.Animation;
+import com.db.chart.view.animation.easing.LinearEase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     BarChartView graph;
     private String[] mLabels= {"W", "I", "L", "L", "I", "A", "M"};
     private float[] mValues = {2.5f, 3.7f, 4f, 8f, 4.5f, 4f, 5f};
-    private int mBarBackgroundColorId = 2;
-    private float mBarCornersSize = 5.54f;
+    private int mBarBackgroundColorId = 1;
+    private float mBarCornersSize = 25.54f; //arredonda as quinas
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +26,20 @@ public class MainActivity extends AppCompatActivity {
         graph = (BarChartView) findViewById(R.id.view);
         graph.reset();
 
-        BarSet dataset = new BarSet(mLabels,mValues);
-        dataset.setColor(10);
+
+        BarSet dataset = new BarSet();
+
+        Bar barra = new Bar("Seg.", 10.5f);
+        barra.setColor(49151);
+
+        dataset.addBar(barra);
+
         graph.addData(dataset);
 
         graph.setBarSpacing(2.3f);
         graph.setBarBackgroundColor(mBarBackgroundColorId);
         graph.setRoundCorners(mBarCornersSize);
         graph.show();
+
     }
 }
